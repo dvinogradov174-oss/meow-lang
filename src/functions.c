e6c395871d61ab40e4fdbe752b911d9834ce81af
Ice748 <dvinogradov174@gmail.com>
2026-05-14 20:00:52 +0300

    Update functions.c
    meow and meowln functions created

--- src/functions.c
+++ src/functions.c
@@ -1,5 +1,21 @@
-#include <meow-lang.h>
+#include "meow-lang.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
 
-void meow(const char *string) {
-  printf("%s", string);
+void meow_lang_error(const char *line, const char *content) {
+  printf("%s\n%s", line, content);
+  _getch();
+  exit(0);
+}
+
+// console
+void meow(const char *str) {
+  printf("%s", str);
+}
+
+void meowln(const char *str) {
+  printf("%s\n", str);
 }

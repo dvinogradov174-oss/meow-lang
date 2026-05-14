0e2a059eebb15dacf8ab50325f96a67591988350
Ice748 <dvinogradov174@gmail.com>
2026-05-14 20:01:19 +0300

    Update meow-lang.h
    meow and meowln functions created

--- src/meow-lang.h
+++ src/meow-lang.h
@@ -1,6 +1,8 @@
 #ifndef MEOW_LANG_H
 #define MEOW_LANG_H
 
-void meow(const char *string);
+void meow_lang_error(const char *line, const char *content);
+void meow(const char *str);
+void meowln(const char *str);
 
 #endif

660e3d37d6ceceef46b3e177e04e45167f681eb4
Ice748 <dvinogradov174@gmail.com>
2026-05-14 20:00:19 +0300

    Update main.c
    meow and meowln functions created!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,77 @@
+#include "meow-lang.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
+
+char buffer[256];
+int console_module = 0;
+
+char *trim(char *str);
+void meow_lang_error(const char *line, const char *content);
+void meow(const char *str);
+void meowln(const char *str);
+void compile_file(const char *file_name);
+
+char *trim(char *str) {
+    char *end;
+    while (isspace((unsigned char)*str))
+        str++;
+
+    if (*str == 0)
+        return str;
+
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
+    end[1] = '\0';
+    return str;
+}
+
+
+void compile_file(const char *file_name) {
+  FILE *file = fopen(file_name, "r");
+  if (file == NULL) {
+    printf("main.meow file not found ):");
+    return;
+  }
+
+  while (fgets(buffer, sizeof(buffer), file)) {
+    char *line = trim(buffer);
+
+      if (strlen(line) == 0 || strncmp(line, "//", 2) == 0)
+          continue;
+
+      if (strcmp(line, "@import> CONSOLE") == 0) {
+          if (console_module == 0) {
+              console_module = 1;
+          } else {
+              meow_lang_error(line, "ERROR 001: This module already imported!");
+          }
+      }
+      else if (strncmp(line, "meow> ", 6) == 0) {
+          if (console_module == 1) {
+              meow(line + 6);
+          } else {
+              meow_lang_error(line, "ERROR 002: CONSOLE module is not imported!");
+          }
+      }
+      else if (strncmp(line, "meowln> ", 8) == 0) {
+          if (console_module == 1) {
+              meowln(line + 8);
+          } else {
+              meow_lang_error(line, "ERROR 002: CONSOLE module is not imported!");
+          }
+      }
+      else {
+          meow_lang_error(line, "ERROR 003: Unknown command!");
+      }
+  }
+
+  fclose(file);
+}
 
 int main(void) {
-  printf("meow");
-  return 0;
+  compile_file("src/main.meow");
 }
